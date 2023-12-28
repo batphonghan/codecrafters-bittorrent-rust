b0e4c85f83f47701ed76a91ae87ae872edbb0562
batphonghan <vanhungdeveloper@gmail.com>
2023-12-28 23:52:40 +0700

    Using clap

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,23 @@
+use anyhow::Ok;
 use hex::encode;
 use peer::HandshakeMessage;
 use serde_json;
-use std::env;
-use tokio::{
-    io::{self, AsyncWriteExt},
-    net::{tcp, TcpStream},
-};
+use std::{env, path::PathBuf};
+use tokio::{io::AsyncWriteExt, net::TcpStream};
+
+use clap::{Parser, Subcommand};
+
 use tracker::{TrackerRequest, TrackerResponse};
 mod meta;
 mod peer;
 mod tracker;
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: SubCommands,
+}
 
 #[allow(dead_code)]
 fn decode_bencoded_value(encoded_value: &str) -> (serde_json::Value, &str) {
@@ -73,20 +81,47 @@ fn decode_bencoded_value(encoded_value: &str) -> (serde_json::Value, &str) {
     return (serde_json::Value::Null, "");
 }
 
+#[derive(Subcommand)]
+#[clap(rename_all = "snake_case")]
+enum SubCommands {
+    /// does testing things
+    Decode {
+        value: String,
+    },
+    Info {
+        path: PathBuf,
+    },
+    Peers {
+        path: PathBuf,
+    },
+    Handshake {
+        path: PathBuf,
+        peer: String,
+    },
+    DownloadPiece {
+        #[arg(short)]
+        output: PathBuf,
+        path: PathBuf,
+        piece: usize,
+    },
+}
+
 // Usage: your_bittorrent.sh decode "<encoded_value>"
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    let args: Vec<String> = env::args().collect();
-    let command = &args[1];
+    let cli = Cli::parse();
 
-    match command.as_str() {
-        "decode" => {
-            let encoded_value = &args[2];
+    let args: Vec<String> = env::args().collect();
+
+    match &cli.command {
+        SubCommands::Decode {
+            value: encoded_value,
+        } => {
             let decoded_value = decode_bencoded_value(encoded_value);
             println!("{}", decoded_value.0.to_string());
         }
-        "info" => {
-            let data = std::fs::read(&args[2]).expect("torrent file exist");
+        SubCommands::Info { path } => {
+            let data = std::fs::read(path).expect("torrent file exist");
 
             let meta: meta::MetaInfo = serde_bencode::from_bytes(&data).expect("Meta");
 
@@ -100,32 +135,20 @@ async fn main() -> anyhow::Result<()> {
             println!("Piece Hashes:");
             meta.pieces_hash().iter().for_each(|v| println!("{v}"));
         }
-        "peers" => {
-            let data = std::fs::read(&args[2]).expect("torrent file exist");
-
-            let meta: meta::MetaInfo = serde_bencode::from_bytes(&data).expect("Meta");
-
-            let mut url = reqwest::Url::parse(&meta.announce).expect("announce URL ");
-
-            let encode_query = TrackerRequest::from(meta).query();
-            url.set_query(Some(&encode_query));
-            let b = reqwest::get(url).await?.bytes().await?;
-
-            let resp: TrackerResponse = serde_bencode::from_bytes(&b).expect("Tracker response");
-
+        SubCommands::Peers { path } => {
+            let resp = get_tracker(path).await?;
             resp.peers()
                 .iter()
                 .for_each(|v| println!("{}:{}", v.0, v.1));
         }
-        "handshake" => {
-            let data = std::fs::read(&args[2]).expect("torrent file exist");
+
+        SubCommands::Handshake { path, peer } => {
+            let data = std::fs::read(path).expect("torrent file exist");
 
             let meta: meta::MetaInfo = serde_bencode::from_bytes(&data).expect("Meta");
 
-            let host = &args[3];
-
             // Connect to a peer
-            let mut stream = TcpStream::connect(host).await?;
+            let mut stream = TcpStream::connect(peer).await?;
 
             let handshake = HandshakeMessage::from(meta);
             // Write some data.
@@ -145,10 +168,33 @@ async fn main() -> anyhow::Result<()> {
             let peer_id = &handshake_bytes[48..];
             println!("Peer ID: {}", encode(peer_id));
         }
+        SubCommands::DownloadPiece {
+            output,
+            path,
+            piece,
+        } => {
+            println!("{:?} {:?} {:?}", output, path, piece);
+        }
         _ => {
             println!("unknown command: {}", args[1])
         }
     }
 
     Ok(())
+}
+
+async fn get_tracker(path: &PathBuf) -> anyhow::Result<TrackerResponse> {
+    let data = std::fs::read(path).expect("torrent file exist");
+
+    let meta: meta::MetaInfo = serde_bencode::from_bytes(&data).expect("Meta");
+
+    let mut url = reqwest::Url::parse(&meta.announce).expect("announce URL ");
+
+    let encode_query = TrackerRequest::from(meta).query();
+    url.set_query(Some(&encode_query));
+    let b = reqwest::get(url).await?.bytes().await?;
+
+    let resp: TrackerResponse = serde_bencode::from_bytes(&b).expect("Tracker response");
+
+    Ok(resp)
 }
